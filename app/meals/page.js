18f94619395b9dfbc985707536b0df3b56a77445
rana awais <=>
2024-11-20 15:19:57 +0500

    set meals pages

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,27 @@
-const MealPage = () => {
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+const MealsPage = () => {
   return (
-    <h1 style={{ color: "white", textAlign: "center" }}>This is meal page</h1>
+    <>
+      <h1 style={{ color: "white", textAlign: "center" }}>This is meal page</h1>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose you favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
-export default MealPage;
+export default MealsPage;
