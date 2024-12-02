70f5b13747a2ecfd35d42bc3b35014445a7fea79
rana awais <=>
2024-12-02 16:11:01 +0500

    managing the form submission status with use form status

--- app/meals/layout.js
+++
@@ -1,9 +0,0 @@
-const MealsLayout = ({ children }) => {
-  return (
-    <>
-      <p> Meals layout</p>
-      {children}
-    </>
-  );
-};
-export default MealsLayout;
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 // Server Action to handle form submission
 
@@ -44,7 +45,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
