c5cd82b36d3636683258069f3327097ebb406dad
rana awais <=>
2024-12-03 18:06:21 +0500

    replace ui with ul

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -2,13 +2,13 @@ import MealItem from "./meal-item";
 import classes from "./meals-grid.module.css";
 const MealsGrid = ({ meals }) => {
   return (
-    <ui className={classes.meals}>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
           <MealItem {...meal} />
         </li>
       ))}
-    </ui>
+    </ul>
   );
 };
 export default MealsGrid;
